8f48b31bf4b9959c793645e21f5e567934621e2e
RaissaMariaB <raissamariab@gmail.com>
2019-10-28 13:31:43 -0300

    adiciona resoluçaõ de exercício de revisão 1 e 2

--- 00a_revisao/index.js
+++ 00a_revisao/index.js
@@ -1,7 +1,38 @@
 // 1.
-// [] adicionar a tag <script> no html
-// [] verificar se está lendo index.js
-// [] alterar cor de <body>
-// [] escutar o clique do botão
-// [] criar função de alterar a cor usando rgb()
-// [] usar Math.random() e Math.floor() ou parseInt()
+// [x] adicionar a tag <script> no html
+// [x] verificar se está lendo index.js
+// [x] alterar cor de <body>
+// [x] escutar o clique do botão
+// [x] criar função de alterar a cor usando rgb()
+// [x] usar Math.random() e Math.floor() ou parseInt()
+
+
+// console.log("olar");
+// let body = document.querySelector("body")
+// const body = document.getElementsByTagName("body")
+
+// console.log(body);
+
+
+const body = document.body
+const btn = document.getElementById("trocaCor")
+// body.style.backgroundColor = "purple"
+
+btn.addEventListener("click", function(){
+    let R = parseInt(Math.random() *255)
+    let G = parseInt(Math.random() *255)
+    let B = parseInt(Math.random() *255)
+    let RGB = "RGB(" + R + " , " + G +" , " + B  + ")"
+
+    body.style.backgroundColor = RGB
+
+    console.log(RGB); 
+    
+})
+
+
+
+
+
+
+console.log("entrei na função");
--- 00b_revisao/index.js
+++ 00b_revisao/index.js
@@ -1,10 +1,10 @@
 // 1.
-// [] adicionar a tag <script> no html
-// [] verificar se está lendo index.js
-// [] alterar cor de <body>
-// [] escutar o clique do botão
-// [] criar função de alterar a cor usando rgb()
-// [] usar Math.random() e Math.floor() ou parseInt()
+// [x] adicionar a tag <script> no html
+// [x] verificar se está lendo index.js
+// [x] alterar cor de <body>
+// [x] escutar o clique do botão
+// [x] criar função de alterar a cor usando rgb()
+// [x] usar Math.random() e Math.floor() ou parseInt()
 
 // ------------------------------------------------------------------------
 // 2. melhorando o código
@@ -12,3 +12,27 @@
 // [] anonymous function
 // [] arrow function
 // [] template string
+
+
+const body = document.body
+const btn = document.getElementById("trocaCor")
+// body.style.backgroundColor = "purple"
+
+function mudaCor(){
+    let number = parseInt(Math.random() *255)
+
+    return number
+}
+
+btn.addEventListener("click", function(){
+    let R = mudaCor() //parseInt(Math.random() *255)
+    let G =  mudaCor()// parseInt(Math.random() *255)
+    let B = mudaCor() //parseInt(Math.random() *255)
+    let RGB = "RGB(" + R + " , " + G +" , " + B  + ")"
+
+    body.style.backgroundColor = RGB
+
+    console.log(RGB); 
+    
+})
+
