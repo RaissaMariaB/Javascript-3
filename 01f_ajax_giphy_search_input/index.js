ff1b0e818d016133fdbf4af2c674eb1638b1f457
RaissaMariab <raissamaria.sb@gmail.com>
2019-10-31 16:45:20 -0300

    Adiciona exercico 04b fetch

--- 01f_ajax_giphy_search_input/index.js
+++ 01f_ajax_giphy_search_input/index.js
@@ -1,3 +1,65 @@
 // XMLHttpRequest na rota search capturando informação do input
 // trazer 10 imagens
 // apresentar as imagens na div #root
+
+
+
+const input = document.getElementById("gifInput")
+const form = document.getElementById("form")
+const div = document.getElementById("root")
+
+
+form.addEventListener("submit", function(evento){
+    
+    evento.preventDefault()
+    div.innerHTML = "   "
+    let  valorInput = input.value
+    input.value = ""
+    console.log(valorInput)
+
+    const url = `http://api.giphy.com/v1/gifs/search?q=${valorInput}&api_key=dc6zaTOxFJmzC&limit=10&offset=0`
+    
+    
+    const request = new XMLHttpRequest()
+    request.onreadystatechange = mostraGifs 
+    request.open("GET", url)
+    request.send()
+    
+        function mostraGifs(){
+            if (request.readyState === 4 && request.status === 200) {
+                const response = request.response
+                const json = JSON.parse(response)
+                console.log(json)
+                const arrayData = json.data;
+
+
+                    for (let i = 0; i < arrayData.length; i++) {
+                        const image = arrayData[i].images.original.url;
+                        div.innerHTML += `
+                        <img src=${image}>
+                        `                    
+                }              
+                                 
+            } else {
+                const status = request.status
+                const statusMsg = request.statusText
+                console.log("meu console loggg");                
+                console.error("este é meu console errooouuuu" , status, statusMsg )
+                console.warn("exite um console warnnnn");
+
+                div.innerHTML = `
+                <h1> +
+                `
+                
+
+            }
+    
+    
+        }  
+
+})    
+
+
+
+
+    
--- 04b_fetch_giphy_search/index.js
+++ 04b_fetch_giphy_search/index.js
@@ -5,3 +5,36 @@
 // [] usar o método fetch() para a url da API Giphy
 // [] transformar o dado recebido
 // [] apresentar <img>
+
+
+const input = document.getElementById("gifInput")
+const btn = document.querySelector("#gifButton")
+const div = document.getElementById("root")
+
+btn.addEventListener("click", (e) => fazerFetch(e))
+
+function fazerFetch(event) {
+    event.preventDefault()
+    div.innerHTML = ""
+    input.innerHTML = ""
+    const valorInput = input.value 
+    console.log("lala");
+    
+    const url = `http://api.giphy.com/v1/gifs/search?q=${valorInput}&api_key=dc6zaTOxFJmzC&limit=10&offset=0`
+
+   fetch(url) 
+   .then( resposta => resposta.json())
+   .then( json => mostrarResultado(json))
+   .catch (e => console.error(e))
+   
+   }
+
+   function mostrarResultado(json) {
+      const arr = json.data 
+      arr.forEach(element => {
+          document.createElement("img")
+          const imgPrint = element.images.original.url
+          div.appendChild("imgPrint")
+          
+      });
+   }
